0795d5eab44e0ffe03d3cd6706fda9bd3de89026
unknown <karanjitrishav4@gmail.com>
2020-10-20 07:47:41 +0545

    Added all files

---
+++ JS/TypeEffect.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.typing',{
+    strings:["Student","Pythonist","Developer","ML Enthusiast"],
+    loop:true,
+    typeSpeed: 40,
+    backSpeed: 40,
+    backDelay: 1000,
+});
---
+++ JS/script.js
@@ -0,0 +1,9 @@
+$('.divodd').each(function () {
+    var y = $(document).scrollTop();
+    var t = $(this).parent().offset().top;
+    if (y > t) {
+        $(this).fadeIn();
+    } else {
+        $(this).fadeOut();
+    }
+});
